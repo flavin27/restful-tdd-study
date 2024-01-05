6f30aa5b56ff574af151bbe94074822b904aa07e
Flavio <77132531+flaviodev27@users.noreply.github.com>
2024-01-04 22:35:06 -0300

    feat: created database

---
+++ tests/Feature/database/PostModelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\database;
+
+
+use App\Models\Post;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class PostModelTest extends TestCase
+{
+    use DatabaseMigrations;
+
+
+    public function test_post_can_be_create() {
+        $user = Post::factory()->create([
+            'title' => 'test',
+            'content' => 'content test',
+        ]);
+
+        $this->assertDatabaseCount('posts', 1);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'test',
+            'content' => 'content test',
+        ]);
+    }
+
+    public function test_can_create_multiple_posts() {
+        Post::factory()->count(10)->create();
+
+        $this->assertDatabaseCount('posts', 10);
+    }
+}
